9a772d58db90c6663357f2da20f259e729deebd3
AJ <andrewdsj@hotmail.com>
2023-04-22 21:09:14 -0500

    Initial Commit

---
+++ src/main/java/test/aj/SBPractice/SbPracticeApplication.java
@@ -0,0 +1,14 @@
+package test.aj.SBPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbPracticeApplication.class, args);
+		System.out.println("Hello Andrew!");
+	}
+
+}
