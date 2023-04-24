2032295489d919ed1e9644d5d542115d7960c00f
AJ <andrewdsj@hotmail.com>
2023-04-23 21:00:27 -0500

    SB Actuator 1.22

--- src/main/java/test/aj/SBPractice/SbPracticeApplication.java
+++ src/main/java/test/aj/SBPractice/SbPracticeApplication.java
@@ -1,5 +1,22 @@
 package test.aj.SBPractice;
 
+/*
+1) Create a SB project from start.spring.io, add hello world and run it in IntelliJ
+2) Add an GetMapping for / endpoint in a new Rest controller class and access the app via localhost:8080
+3) Add project.author to app.prop and use @Value("${project.author}") to display it
+4) Check File > Settings > BED > Compiler: Build project automatically
+5) Check File > Settings > Advanced Settings: Allow auto-make...
+6) Add spring-boot-devtools to pom.xml, make changes to ensure hot reload is working.
+7) Add spring-boot-starter-actuator and test /actuator/health
+8) Add the following to app.prop and test out /actuator/info and ensure /actuator/health is still up
+management.endpoints.web.exposure.include=health,info
+management.info.env.enabled=true
+info.app.name=SB Practice
+info.app.description=Test Spring Boot features
+9) change health,info on the line above to * and test out actuator/beans & actuator/mappings
+management.endpoints.web.exposure.include=*
+ */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +25,7 @@ public class SbPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbPracticeApplication.class, args);
-		System.out.println("Hello Andrew!");
+		System.out.println("Hello Andrew!!!");
 	}
 
 }
---
+++ src/main/java/test/aj/SBPractice/rest/RestController.java
@@ -0,0 +1,16 @@
+package test.aj.SBPractice.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @Value("${project.author}")
+    private String firstName;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "App created by " + firstName;
+    }
+}
